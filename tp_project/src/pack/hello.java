98a0a8f073508efacfce8a455af72f159bed957d
manalbenrabhi <150665153+manalbenrabhi@users.noreply.github.com>
2024-02-11 17:20:13 +0100

    hello world onlly

--- tp_project/src/pack/hello.java
+++ tp_project/src/pack/hello.java
@@ -5,8 +5,6 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("New project");
-		Syastem.out.println("Another line in dev branch");
 
 	}
 
