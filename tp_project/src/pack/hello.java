ae94adde90d18efaef58ecc4f12066ba4679d559
manalbenrabhi <150665153+manalbenrabhi@users.noreply.github.com>
2024-02-11 16:20:10 +0100

    update 2 in main

--- tp_project/src/pack/hello.java
+++ tp_project/src/pack/hello.java
@@ -6,6 +6,7 @@ public class hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
 		System.out.println("We are in main branch");
+		System.out.println("Another line in main branch");
 
 	}
 
