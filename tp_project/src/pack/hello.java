b3f1c55e2f985403f868bb976c0927d8854f7ce2
manalbenrabhi <150665153+manalbenrabhi@users.noreply.github.com>
2024-02-11 16:19:33 +0100

    update 1 in main

--- tp_project/src/pack/hello.java
+++ tp_project/src/pack/hello.java
@@ -5,8 +5,7 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("New project");
-		System.out.println("End of project");
+		System.out.println("We are in main branch");
 
 	}
 
